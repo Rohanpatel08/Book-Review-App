abad5fae53a38a4020ab2ac97930a1110ef770a5
Rohanpatel08 <raj.bangoriya08@gmail.com>
2024-09-20 22:25:04 +0530

    profile pic upload added

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -24,16 +24,16 @@
                         Profile
                     </div>
                     @if (Session::has('success'))
-                    <div class="alert alert-success">
+                    <div class="alert alert-success fade show" role="alert">
                         {{Session::get('success')}}
                     </div>
                     @endif
                     @if (Session::has('error'))
-                    <div class="alert alert-danger">
+                    <div class="alert alert-danger fade show" role="alert">
                         {{Session::get('error')}}
                     </div>
                     @endif
-                    <form action="{{route('update.profile')}}" method="post">
+                    <form action="{{route('update.profile')}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="card-body">
                             <div class="mb-3">
@@ -66,6 +66,16 @@
                                 <label for="name" class="form-label">Role</label>
                                 <input type="text" disabled name="role" id="role" value="{{Auth::user()->role}}" class="form-control">
                             </div>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Profile Pic</label>
+                                <input type="file" class="form-control" name="profile" id="profile" class="form-control">
+                                @error('profile')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <img src="{{asset(Auth::user()->profile ? 'images/'.Auth::user()->profile : 'images/default.jpg')}}" alt="profile" id="preview" class="img-thumbnail profile-pic">
+                            </div>
                             <button class="btn btn-primary mt-2">Update</button>
                         </div>
                     </form>
@@ -97,7 +107,22 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script>
+        document.getElementById('profile').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
 
+                // When the file is loaded, set the image src to the file content
+                reader.onload = function(e) {
+                    document.getElementById('preview').src = e.target.result;
+                };
+
+                // Read the file content
+                reader.readAsDataURL(file);
+            }
+        });
+    </script>
 </body>
 
 </html>
